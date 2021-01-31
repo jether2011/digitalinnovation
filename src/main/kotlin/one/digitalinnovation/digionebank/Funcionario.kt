d242e616030aacb547865b8798444a373641f2e0
Jether Rodrigues <jetherrodrigues@gmail.com>
2021-01-31 14:07:50 -0300

    updated employee class

--- src/main/kotlin/one/digitalinnovation/digionebank/Funcionario.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Funcionario.kt
@@ -1,11 +1,16 @@
 package one.digitalinnovation.digionebank
 
-import java.math.BigDecimal
-
 abstract class Funcionario(
     nome: String,
     cpf: String,
-    val salario: BigDecimal,
+    val salario: Double,
 ) : Pessoa(nome, cpf) {
-    abstract fun calculoAuxilio();
+    protected abstract fun calculoAuxilio(): Double
+
+    override fun toString(): String = """
+        Nome: $nome
+        Cpf: $cpf
+        Salario: $salario
+        Auxilio: ${calculoAuxilio()}
+    """.trimIndent()
 }
