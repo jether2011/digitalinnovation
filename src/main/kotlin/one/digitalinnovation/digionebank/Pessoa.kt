f54698766158bc193cec8f43539e0e320927f2ef
Jether Rodrigues <jetherrodrigues@gmail.com>
2021-01-31 12:03:47 -0300

    updated to talk about class members

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -2,15 +2,15 @@ package one.digitalinnovation.digionebank
 
 class Pessoa {
     var nome: String = "Jether"
-
     var cpf: String = "123.123.123-11"
     private set
+
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
 }
 
 fun main() {
     val jether = Pessoa()
-
-    println(jether)
-    println(jether.nome)
-    println(jether.cpf)
+    println(jether.pessoaInfo())
 }
