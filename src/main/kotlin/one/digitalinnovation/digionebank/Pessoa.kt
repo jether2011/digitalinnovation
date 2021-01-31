22c4362139fe8246e3cc2e23886918247739281a
Jether Rodrigues <jetherrodrigues@gmail.com>
2021-01-31 11:54:06 -0300

    updated to apply the private modifier to set function of cpf property

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -2,12 +2,15 @@ package one.digitalinnovation.digionebank
 
 class Pessoa {
     var nome: String = "Jether"
+
     var cpf: String = "123.123.123-11"
+    private set
 }
 
 fun main() {
     val jether = Pessoa()
 
+    println(jether)
     println(jether.nome)
     println(jether.cpf)
 }
