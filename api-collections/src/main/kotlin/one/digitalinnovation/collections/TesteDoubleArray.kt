35592b52b618179727a3d3afee39f47b57749f13
Jether Rodrigues <jetherrodrigues@gmail.com>
2021-02-03 09:18:37 -0300

    created double array and array of test class

---
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteDoubleArray.kt
@@ -0,0 +1,21 @@
+package one.digitalinnovation.collections
+
+fun main() {
+    val salarios = DoubleArray(3)
+    salarios[0] = 1000.0
+    salarios[1] = 3000.0
+    salarios[2] = 500.0
+
+    salarios.forEach { println(it) }
+
+    println("-----------------------------------")
+    salarios.forEachIndexed { index, salario ->
+        salarios[index] = salario * 1.1
+    }
+    salarios.forEach { println(it) }
+
+    println("-----------------------------------")
+    val salarios2 = doubleArrayOf(1500.0, 1250.0, 5000.0)
+    salarios2.sort()
+    salarios2.forEach { println(it) }
+}
