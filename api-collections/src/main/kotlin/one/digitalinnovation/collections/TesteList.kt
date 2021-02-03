c7ed5045caeec314d3348384fc130c265e4336d5
Jether Rodrigues <jetherrodrigues@gmail.com>
2021-02-03 10:29:15 -0300

    created list tests

---
+++ api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
@@ -0,0 +1,36 @@
+package one.digitalinnovation.collections
+
+fun main() {
+    val joao = Funcionario("Joao", 2000.0, "CLT")
+    val pedro = Funcionario("Pedro", 1500.0, "PJ")
+    val maria = Funcionario("Maria", 4000.0, "CLT")
+
+    val funcionarios = listOf(joao, pedro, maria)
+
+    funcionarios.forEach { println(it) }
+
+    println("------------------")
+    println(funcionarios.find { it.nome == "Maria" })
+
+    println("------------------")
+    funcionarios
+        .sortedBy { it.salario }
+        .forEach { println(it) }
+
+    println("------------------")
+    funcionarios
+        .groupBy { it.tipoContratacao }
+        .forEach { println(it) }
+}
+
+data class Funcionario(
+    val nome: String,
+    val salario: Double,
+    val tipoContratacao: String
+) {
+    override fun toString(): String =
+        """
+            Nome:    $nome
+            Salário: $salario
+        """.trimIndent()
+}
